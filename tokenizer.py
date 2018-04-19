bec3ce8ea56ff0811ebf9aeef07b12908a4fd1f6
laviniasalicchi <lavinia.salicchi@libero.it>
2018-04-19 14:40:00 +0200

    roba fatta fin'ora

---
+++ tokenizer.py
@@ -0,0 +1,47 @@
+import tokenize
+import token
+from training_data import getData
+import nltk
+from nltk import trigrams
+import numpy as np
+from collections import Counter
+
+data, target = getData()
+numb = 5
+
+print("---",target[numb]['truthMedian']," ",target[numb]['truthClass'])
+
+feature = data[numb]['targetTitle']
+print(feature)
+
+splitter = nltk.data.load('tokenizers/punkt/english.pickle')
+sent_pos = {'id': data[numb]['id']}
+sent_only_pos = []
+
+if len(feature[0]) == 1:
+    for sent in splitter.tokenize(feature.strip()):
+        tokenizer = nltk.word_tokenize(sent)
+        pos_tags = nltk.pos_tag(tokenizer)
+        for p in pos_tags:
+            sent_only_pos.append(p[1])
+        sent_pos.update({'pos_tags':sent_only_pos})
+        trigrams_list = list(trigrams(tokenizer))
+    print(sent_pos)
+else:
+    a=1
+    for line in feature:
+        for sent in splitter.tokenize(line.strip()):
+            tokenizer = nltk.word_tokenize(sent)
+            pos_tags = nltk.pos_tag(tokenizer)
+            a = a+1
+            for p in pos_tags:
+                sent_only_pos.append(p[1])
+            sent_pos.update({'pos_tags': sent_only_pos})
+            trigrams_list = list(trigrams(tokenizer))
+    print(sent_pos)
+
+print("*********** TRIGRAMMI POS ***********")
+trigrams_pos = list(trigrams(sent_pos['pos_tags']))
+print(trigrams_pos)
+
+
---
+++ training_data.py
@@ -0,0 +1,56 @@
+import json
+import jsonlines
+import io
+import pickle
+import numpy as np
+import tokenize
+
+
+def getData():
+    datas = []
+    target = []
+
+    '''{
+        "id": "<instance id>",
+        "postTimestamp": "<weekday> <month> <day> <hour>:<minute>:<second> <time_offset> <year>",
+        "postText": ["<text of the post with links removed>"],
+        "postMedia": ["<path to a file in the media archive>"],
+        "targetTitle": "<title of target article>",
+        "targetDescription": "<description tag of target article>",
+        "targetKeywords": "<keywords tag of target article>",
+        "targetParagraphs": ["<text of the ith paragraph in the target article>"],
+        "targetCaptions": ["<caption of the ith image in the target article>"]
+    }'''
+
+    file_inst = 'datasets/1/instances.jsonl'
+    with jsonlines.open(file_inst) as reader:
+        for instances in reader:
+            datas.append(instances)
+
+
+
+
+    '''{
+        "id": "<instance id>",
+        "truthJudgments": [<number in [0,1]>],
+        "truthMean": <number in [0,1]>,
+        "truthMedian": <number in [0,1]>,
+        "truthMode": <number in [0,1]>,
+        "truthClass": "clickbait | no-clickbait"
+      }'''
+
+    file_truth = 'datasets/1/truth.jsonl'
+    with jsonlines.open(file_truth) as reader:
+        i=0
+        for truths in reader:
+            target.append(truths)
+            '''if target[i]['truthClass'] == 'clickbait':
+                print(i)
+                print(datas[i])
+            '''
+            i=i+1
+
+
+    return datas, target
+
+getData()
